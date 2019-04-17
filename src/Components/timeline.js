cca39eb0aebb6c653541d881a95c9171aa413252
MeharKaurAhuja <mehar1520.cse16@chitkara.edu.in>
2019-04-17 12:35:35 +0530

    Ex:Version3

--- src/Components/timeline.js
+++ src/Components/timeline.js
@@ -5,29 +5,43 @@ import Comment from './Comment';
 import AddComment from './AddComment';
 import '../App.css';
 
-class Timeline extends Component {
+
+class Timeline extends Component {   
     state = {
         img1: [1,219,'LIKE'],
         img2: [2,167,'LIKE'],
         img3: [3,200,'LIKE'],
         comments: [
           { name: 'Jim', text:'I love this place', id: 1 }
-        ]
+        ],
+        showComm: false
+    }
+    handleComm = () => {
+        if(this.state.showComm===true){
+            this.setState({
+                showComm:false
+            })
+        }
+        else{
+            this.setState({
+                showComm:true
+            })
+        }
     }
 
     handleClick = (e,param) => {
-       console.log('Clicked', e[0]);
+       //console.log('Clicked', e[0]);
        
         
         if(e[2]==='LIKE'){
             const inc = e[1]+1;
             
-            if(e[0]==1){
+            if(e[0]===1){
                 this.setState({
                     img1: [1,inc,'UNLIKE']
                 })
             }
-            else if(e[0]==2){
+            else if(e[0]===2){
                 this.setState({
                     img2: [2,inc,'UNLIKE']
                 })  
@@ -41,12 +55,12 @@ class Timeline extends Component {
         
         else{
             const dec = e[1]-1;
-            if(e[0]==1){
+            if(e[0]===1){
                 this.setState({
                     img1: [1,dec,'LIKE']
                 })  
             }
-            else if(e[0]==2){
+            else if(e[0]===2){
                 this.setState({
                     img2: [2,dec,'LIKE']
                 })  
@@ -71,44 +85,68 @@ class Timeline extends Component {
        
     render() {
         return (
+        <div className="row">
+            <div className='col s7 offset-s1 card card2'>
+                <div className="timeline-page">
+                    <h1>Hey there!</h1> <br />
+                    <img className="time" src="https://i.imgur.com/5G0SJtn.jpg" alt="oops! not available" />
+                    <br />
+                    <br />
+                
+                    <input type="button" value={this.state.img1[2]} className="like" onClick={this.handleClick.bind(this,this.state.img1)} />
+                    <span className='like'> {this.state.img1[1]} </span>
+
+                    <input type="button" value="Show Comments" className='like' onClick={this.handleComm}/>
+                    {this.state.showComm ? (
+                        <div>
+                            <Comment comments = {this.state.comments}/><br />
+                        
+                        </div>) :
+                    (null)}
+                    <AddComment addComment={this.addComment} /><br /><br />
+                
+                    <br />
+                    <br />
+                    <img className="time" src="https://i.imgur.com/5G0SJtn.jpg" alt="oops! not available" />
+                    <br />
+                    <br />
+                
+                    <input type="button" value={this.state.img2[2]} className="like" onClick={this.handleClick.bind(this,this.state.img2)} />
+                    <span className="like"> {this.state.img2[1]} </span>
+                    <input type="button" value="Show Comments" className='like' onClick={this.handleComm}/>
+                    {this.state.showComm ? (
+                        <div>
+                            <Comment comments = {this.state.comments}/><br />
+                        
+                        </div>) :
+                    (null)}
+                    <AddComment addComment={this.addComment} /><br /><br />
+                
+                    <br />
+                    <br />
+                    <img className="time" src="https://i.imgur.com/5G0SJtn.jpg" alt="oops! not available" />
+                    <br />
+                    <br />
+                
+                    <input type="button" value={this.state.img3[2]} className="like" onClick={this.handleClick.bind(this,this.state.img3)} />
+                    <span className='like'> {this.state.img3[1]} </span>
+                    <input type="button" value="Show Comments" className='like' onClick={this.handleComm}/>
+                    {this.state.showComm ? (
+                        <div>
+                            <Comment comments = {this.state.comments}/><br />
+                        
+                        </div>) :
+                    (null)}
+                    <AddComment addComment={this.addComment} /><br /><br />
+                
+                    <br />
+                    <br />
+                
+                </div>
+            </div>
             
-            <div className='card card2'>
-            <div class="timeline-page">
-                <h1>Hey there!</h1> <br />
-                <img className="time" src="https://i.imgur.com/5G0SJtn.jpg" alt="oops! not available" />
-                <br />
-                <br />
-                
-                <input type="button" value={this.state.img1[2]} className="like" onClick={this.handleClick.bind(this,this.state.img1)} />
-                <span className="likes">{this.state.img1[1]}</span>
-                <AddComment addComment={this.addComment} /><br /><br />
-                <Comment comments={this.state.comments}/><br />
-                <br />
-                <br />
-                <img className="time" src="https://i.imgur.com/5G0SJtn.jpg" alt="oops! not available" />
-                <br />
-                <br />
-                
-                <input type="button" value={this.state.img2[2]} className="like" onClick={this.handleClick.bind(this,this.state.img2)} />
-                <span className="likes">{this.state.img2[1]}</span>
-                <AddComment addComment={this.addComment} /><br /><br />
-                <Comment comments={this.state.comments}/><br />
-                <br />
-                <br />
-                <img className="time" src="https://i.imgur.com/5G0SJtn.jpg" alt="oops! not available" />
-                <br />
-                <br />
-                
-                <input type="button" value={this.state.img3[2]} className="like" onClick={this.handleClick.bind(this,this.state.img3)} />
-                <span className="likes">{this.state.img3[1]}</span>
-                <AddComment addComment={this.addComment} /><br /><br />
-                <Comment comments={this.state.comments}/><br />
-                <br />
-                <br />
-                
-            </div>
-            </div>
-                
+            
+        </div>
         )
     }
 }
